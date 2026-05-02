83f0a56face611958c5978d1777512f2597879e8
ronyislam502 <ronyislam502@gmail.com>
2026-05-02 20:01:34 +0600

    email

---
+++ src/app/shared/jwtHelpers.ts
@@ -0,0 +1,13 @@
+import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
+
+export const createToken = (
+  jwtPayload: { email: string; role: string },
+  secret: string,
+  expiresIn: SignOptions["expiresIn"]
+) => {
+  return jwt.sign(jwtPayload, secret, { expiresIn });
+};
+
+export const verifyToken = (token: string, secret: string) => {
+  return jwt.verify(token, secret) as JwtPayload;
+};
